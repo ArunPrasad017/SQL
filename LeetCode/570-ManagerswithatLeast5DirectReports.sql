38b061c271e365c63d11b05cc6d059a8cfe23988
aralagarsamy <aralagarsamy@egencia.com>
2020-09-15 23:45:46 -0700

    adde sql files

---
+++ LeetCode/570-ManagerswithatLeast5DirectReports.sql
@@ -0,0 +1,32 @@
+/*
+The Employee table holds all employees including their managers. Every employee has an Id, and there is also a column for the manager Id.
+
++------+----------+-----------+----------+
+|Id    |Name 	  |Department |ManagerId |
++------+----------+-----------+----------+
+|101   |John 	  |A 	      |null      |
+|102   |Dan 	  |A 	      |101       |
+|103   |James 	  |A 	      |101       |
+|104   |Amy 	  |A 	      |101       |
+|105   |Anne 	  |A 	      |101       |
+|106   |Ron 	  |B 	      |101       |
++------+----------+-----------+----------+
+Given the Employee table, write a SQL query that finds out managers with at least 5 direct report. For the above table, your SQL query should return:
+
++-------+
+| Name  |
++-------+
+| John  |
++-------+
+Note:
+No one would report to himself.
+*/
+
+/* Write your T-SQL query statement below */
+select e1.name from Employee e1
+inner join
+(select ManagerId as ID
+from Employee
+group by ManagerId
+having count(*)>=5)e2
+on e1.ID = e2.ID
