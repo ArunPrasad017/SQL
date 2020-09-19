443e882be9c0240d2767185fa1c6848ed25a4d4d
aralagarsamy <aralagarsamy@egencia.com>
2020-09-18 19:51:37 -0700

    added sql programs done

---
+++ LeetCode/1070-ProductSalesAnalysisIII.sql
@@ -0,0 +1,44 @@
+/*
+Write an SQL query that selects the product id, year, quantity, 
+and price for the first year of every product sold.
+
+The query result format is in the following example:
+
+Sales table:
++---------+------------+------+----------+-------+
+| sale_id | product_id | year | quantity | price |
++---------+------------+------+----------+-------+ 
+| 1       | 100        | 2008 | 10       | 5000  |
+| 2       | 100        | 2009 | 12       | 5000  |
+| 7       | 200        | 2011 | 15       | 9000  |
++---------+------------+------+----------+-------+
+
+Product table:
++------------+--------------+
+| product_id | product_name |
++------------+--------------+
+| 100        | Nokia        |
+| 200        | Apple        |
+| 300        | Samsung      |
++------------+--------------+
+
+Result table:
++------------+------------+----------+-------+
+| product_id | first_year | quantity | price |
++------------+------------+----------+-------+ 
+| 100        | 2008       | 10       | 5000  |
+| 200        | 2011       | 15       | 9000  |
++------------+------------+----------+-------+
+*/
+
+select product_id,year as first_year, quantity, price
+from 
+(
+    select product_id,
+    year,
+    quantity,
+    price,
+    rank() over (partition by product_id order by year) as rnk
+    from Sales
+)t
+where t.rnk = 1
